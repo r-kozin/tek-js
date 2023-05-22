28a0cab5f97db11bb9a632c099a926380c7b6a96
Ryan Kozin <ryankozin@Ryans-MacBook-Pro.local>
2023-05-22 10:06:25 -0700

    first commit

---
+++ js/app.js
@@ -0,0 +1,18 @@
+console.log('Hello World!');
+console.log('you are in the app.js');
+
+let num = 5;
+let name = 'John';
+let name2 = null;
+let name3 = undefined;
+let objects = {
+    name: "jim",
+    age: 25,
+    name2: "james"
+}
+
+console.log(num);
+console.log(name);
+console.log(name2);
+console.log(name3);
+console.log(objects);
